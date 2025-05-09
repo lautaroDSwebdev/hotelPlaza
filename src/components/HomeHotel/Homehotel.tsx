33e263a2ff4e3b028f233c2c8ad83d5a0bdb0fad
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-05-09 02:01:09 -0300

    add: more font size on plaza hotel title home

--- src/components/HomeHotel/Homehotel.tsx
+++ src/components/HomeHotel/Homehotel.tsx
@@ -20,7 +20,7 @@ export const Homehotel = () => {
         </h1>
         <div className=" ">
           <p className="g-paragrapg-size">
-            <b className="g-textgcolor-page g-contorno-texto text-[1.5rem]">El Plaza Hotel</b> está ubicado estratégicamente en
+            <b className="g-textgcolor-page g-contorno-texto text-[2rem]">El Plaza Hotel</b> está ubicado estratégicamente en
             el corazón de la ciudad de Tandil. Esta ubicación -frente a la plaza
             central le permite a los pasajeros una rápida conexión con los
             paseos típicos de la ciudad, el casino, el lago, las sierras
