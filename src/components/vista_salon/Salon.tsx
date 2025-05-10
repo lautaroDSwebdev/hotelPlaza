c26fec2af37785006897ec5d90e27c1ab2aef58e
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-05-09 23:08:55 -0300

    fix: titles with mediaqueries and text elipsis

--- src/components/vista_salon/Salon.tsx
+++ src/components/vista_salon/Salon.tsx
@@ -14,7 +14,7 @@ const Salon = () => {
             <article className='g-height-header max-with-viewp min-h-screen'>
                 <img className='g-image-header' src={imagen_salon} alt='imagen restoran' />
                 <h1 className='g-titles-h1'>Salones</h1>
-                <div className='p-[2rem] '>
+                <div className=' '>
                     <p className='g-paragrapg-size'> El Restaurant del Plaza Hotel le ofrece diariamente un menú ejecutivo con opciones que usted podrá acomodar a sus horarios y reuniones de trabajo.</p>
                     <p className='g-paragrapg-size '>No solo los pasajeros del hotel pueden deleitarse con la cocina internacional propuesta por el chef ya que el restaurant es abierto al público en general y en su cálido ambiente usted podrá disfrutar -en familia o entre amigos- de una amplia gama de platos y las sugerencias para todos los gustos.</p>
                 </div>
