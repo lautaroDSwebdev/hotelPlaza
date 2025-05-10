1b920eb3c80678cbf5d95781994b82536cf6cd12
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-05-10 01:17:16 -0300

    feat: my copyright  on footer

--- src/components/fotter/Fotter.tsx
+++ src/components/fotter/Fotter.tsx
@@ -9,38 +9,46 @@ import Link from 'next/link';
 const FotterComponent = () => {
   return (
     <footer className='g-bgcolor-page'>
-      <section className='max-with-viewp'>
-        <div>
-          <a target='_blank' href="mailto:plazahoteltandil@gmail.com">
-            <CgMail className='mail' />
-          </a>
-          <b>plazahoteltandil@gmail.com</b>
-        </div>
-        <div>
-          <a target='_blank' href="https://api.whatsapp.com/send?phone=2494427160">
-            <BsWhatsapp className='wsp'/>
-          </a>
-          <b>(0249)4427160/80/89</b>
-        </div>
-        <div>
-          <Link href={`/ubicacion`}>
-            <IoLocationOutline className='loc' />
-          </Link>
-          <b>General Pinto 438 Tandil</b>
-        </div>
-        <div>
-          <a target='_blank' href="https://www.facebook.com/PlazaHotel.Tandil">
-            <TbBrandFacebook className='facebk' />
-          </a>
-          <b> </b>
-        </div>
-        <div>
-          <a target='_blank' href="https://www.instagram.com/plazahoteltandil">
-            <RiInstagramLine className='ig'/>
-          </a>
-          <b> </b>
-        </div>
-      </section>
+
+      <article className='max-width-footer '>
+        <section className='section_contact'>
+          <div>
+            <a target='_blank' href="mailto:plazahoteltandil@gmail.com">
+              <CgMail className='mail' />
+            </a>
+            <b>plazahoteltandil@gmail.com</b>
+          </div>
+          <div>
+            <a target='_blank' href="https://api.whatsapp.com/send?phone=2494427160">
+              <BsWhatsapp className='wsp' />
+            </a>
+            <b>(0249)4427160/80/89</b>
+          </div>
+          <div>
+            <Link href={`/ubicacion`}>
+              <IoLocationOutline className='loc' />
+            </Link>
+            <b>General Pinto 438 Tandil</b>
+          </div>
+          <div>
+            <a target='_blank' href="https://www.facebook.com/PlazaHotel.Tandil">
+              <TbBrandFacebook className='facebk' />
+            </a>
+            <b> </b>
+          </div>
+          <div>
+            <a target='_blank' href="https://www.instagram.com/plazahoteltandil">
+              <RiInstagramLine className='ig' />
+            </a>
+            <b> </b>
+          </div>
+        </section>
+        <section className='section_copyright'>
+          <p>
+           &copy; 2025 Lautaro Di Salvo Frontend Developer
+          </p>
+        </section>
+      </article>
     </footer>
   )
 }
